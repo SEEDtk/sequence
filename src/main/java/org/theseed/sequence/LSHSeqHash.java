8be2c091d46152ed99da6acd88a6a62b6135d732
Bruce Parrello <bparrello@figresearch.com>
2020-04-05 06:36:39 -0500

    First working version

---
+++ src/main/java/org/theseed/sequence/LSHSeqHash.java
@@ -0,0 +1,307 @@
+/**
+ *
+ */
+package org.theseed.sequence;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import info.debatty.java.lsh.LSH;
+import info.debatty.java.lsh.MinHash;
+
+/**
+ * This is a locality-sensitive hash that maps sequences to strings using a Mash/MinHash approach.  The sequence
+ * is converted into a sketch using the Mash algorithm.  Each sketch is converted into an array of bucket numbers.
+ * If the sequence is being inserted, it is placed into the matching bucket at each stage.  If the sequence is being
+ * used for a search, we examine the matching bucket at each stage and do direct comparisons to find the closest
+ * sequences.
+ *
+ * @author Bruce Parrello
+ *
+ */
+public class LSHSeqHash extends LSH {
+
+    /**
+     * serialization class ID
+     */
+    private static final long serialVersionUID = -4410879652283658775L;
+
+    /**
+     * This subclass contains a sequence key and its connected object.
+     */
+    private class Entry {
+        /** sketch for this entry's sequence */
+        private int[] sketch;
+        /** target object associated with the sequence */
+        private String name;
+
+        /**
+         * Construct a hash entry.
+         *
+         * @param seq		kmer object to use for comparison
+         * @param string	string associated with the sequence that generated the kmers
+         */
+        public Entry(int[] sk, String string) {
+            this.sketch = sk;
+            this.name = string;
+        }
+
+        /**
+         * @return the distance between another sequence and this entry's sequence
+         *
+         * @param otherSketch	sketch of the other sequence
+         */
+        public double distance(int[] otherSketch) {
+            return LSHSeqHash.this.sketchDistance(this.sketch, otherSketch);
+        }
+
+        /**
+         * @return the sequence target
+         */
+        public String getName() {
+            return name;
+        }
+
+    }
+
+    /**
+     * This subclass forms a bucket.
+     */
+    private static class Bucket implements Iterable<Entry> {
+        private List<Entry> entries;
+
+        /**
+         * Create an empty bucket.
+         */
+        public Bucket() {
+            this.entries = new LinkedList<Entry>();
+        }
+
+        /**
+         * Add an entry to a bucket.
+         *
+         * @param entry		entry to add
+         */
+        public void add(Entry entry) {
+            this.entries.add(entry);
+        }
+
+        @Override
+        public Iterator<Entry> iterator() {
+            return entries.iterator();
+        }
+
+    }
+
+    /**
+     * This subclass returns a result.  It contains the distance and the target (sequence ID),
+     * and is sorted by distance followed by sequence ID.
+     */
+    public static class Result implements Comparable<Result> {
+
+        private double distance;
+        private String target;
+
+        /**
+         * Create a new result object.
+         *
+         * @param distance		distance to the search sequence
+         * @param target		target (sequence ID) of the found sequence
+         */
+        private Result(double distance, String target) {
+            this.distance = distance;
+            this.target = target;
+        }
+
+        /**
+         * Merge a new result into a result set.
+         *
+         * @param results	sorted result set to merge into
+         * @param maxSize	maximum number of items allowed in result set
+         * @param dist		distance from search sequence to found sequence
+         * @param id		target (sequence ID) of found sequence
+         *
+         * @return TRUE if the new result is accepted, else FALSE
+         */
+        public static boolean merge(SortedSet<Result> results, int maxSize, double dist, String id) {
+            boolean retVal = false;
+            Result next = new Result(dist, id);
+            if (results.size() < maxSize) {
+                results.add(next);
+                retVal = true;
+            } else {
+                Result last = results.last();
+                if (last.compareTo(next) > 0) {
+                    results.remove(last);
+                    results.add(next);
+                    retVal = true;
+                }
+            }
+            return retVal;
+        }
+
+        @Override
+        public int compareTo(Result o) {
+            int retVal = Double.compare(this.distance, o.distance);
+            if (retVal == 0)
+                retVal = this.target.compareTo(o.target);
+            return retVal;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            long temp;
+            temp = Double.doubleToLongBits(distance);
+            result = prime * result + (int) (temp ^ (temp >>> 32));
+            result = prime * result + ((target == null) ? 0 : target.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Result other = (Result) obj;
+            if (Double.doubleToLongBits(distance) != Double.doubleToLongBits(other.distance))
+                return false;
+            if (target == null) {
+                if (other.target != null)
+                    return false;
+            } else if (!target.equals(other.target))
+                return false;
+            return true;
+        }
+
+        /**
+         * @return the distance from the search sequence to this result
+         */
+        public double getDistance() {
+            return distance;
+        }
+
+        /**
+         * @return the target (sequence ID) of this result
+         */
+        public String getTarget() {
+            return target;
+        }
+
+    }
+
+    // FIELDS
+
+    /** number of stages */
+    private int stages;
+
+    /** number of objects stored */
+    private int size;
+
+    /** master hash table (first index is stage, second is bucket */
+    private Bucket[][] masterTable;
+
+    /** minhash object for creating sketches */
+    private MinHash minHasher;
+
+    /**
+     * Construct a blank, empty sequence hash.
+     *
+     * @param w		width of signatures
+     * @param s		number of stages
+     * @param b		number of buckets
+     * @param seed	random number seed
+     */
+    public LSHSeqHash(int w, int s, int b, long seed) {
+        super(s, b);
+        this.stages = s;
+        this.size = 0;
+        this.masterTable = new Bucket[s][b];
+        for (int si = 0; si < s; si++)
+            for (int bi = 0; bi < b; bi++)
+                this.masterTable[si][bi] = new Bucket();
+        this.minHasher = new MinHash(w, SequenceKmers.DICT_SIZE, seed);
+    }
+
+
+    /**
+     * @return the distance between two sketches
+     *
+     * @param sketch		first sketch
+     * @param otherSketch	second sketch
+     */
+    protected double sketchDistance(int[] sketch, int[] otherSketch) {
+        return 1.0 - minHasher.similarity(sketch, otherSketch);
+    }
+
+    /**
+     * @return the estimated distance between two sequences using sketches
+     *
+     * @param kmers		kmers for the first sequence
+     * @param other		kmers for the second sequence
+     */
+    public double testSketches(SequenceKmers kmers, SequenceKmers other) {
+        int[] sketch = this.minHasher.signature(kmers.hashSet());
+        int[] otherSketch = this.minHasher.signature(other.hashSet());
+        return sketchDistance(sketch, otherSketch);
+    }
+
+    /**
+     * Add a sequence to the hash.
+     *
+     * @param seqKmers	kmer object for the sequence
+     * @param target	string representing the target for the sequence
+     */
+    public void add(SequenceKmers seqKmers, String target) {
+        // Compute the sketch and the bucket array for the new sequence.
+        int[] sketch = this.minHasher.signature(seqKmers.hashSet());
+        int[] buckets = this.hashSignature(sketch);
+        // Place this target in the appropriate buckets.
+        Entry entry = new Entry(sketch, target);
+        for (int i = 0; i < this.stages; i++)
+            this.masterTable[i][buckets[i]].add(entry);
+        // Record the new entry.
+        this.size++;
+    }
+
+    /**
+     * Search for the N closest sequences to an input sequence
+     *
+     * @param seqKmers	the search sequence of interest
+     * @param n			the maximum number of results to return
+     * @param maxDist	the maximum allowable distance
+     *
+     * @return the N closest sequence results in a sorted set
+     */
+    public SortedSet<Result> getClosest(SequenceKmers seqKmers, int n,
+            double maxDist) {
+        SortedSet<Result> retVal = new TreeSet<Result>();
+        // Compute the sketch and the bucket array for the search sequence.
+        int[] sketch = this.minHasher.signature(seqKmers.hashSet());
+        int[] buckets = this.hashSignature(sketch);
+        // Check each bucket for close sequences.
+        for (int i = 0; i < this.stages; i++) {
+            for (Entry entry : this.masterTable[i][buckets[i]]) {
+                double dist = entry.distance(sketch);
+                if (dist <= maxDist)
+                    Result.merge(retVal, n, dist, entry.getName());
+            }
+        }
+        return retVal;
+    }
+
+    /**
+     * @return the number of entries in this table
+     */
+    public int size() {
+        return this.size;
+    }
+
+}
--- src/test/java/org/theseed/sequence/SeqTest.java
+++ src/test/java/org/theseed/sequence/SeqTest.java
@@ -10,6 +10,8 @@ import junit.framework.TestSuite;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+import java.util.SortedSet;
+
 /**
  * Unit test for minhash stuff.
  *
@@ -36,8 +38,95 @@ public class SeqTest extends TestCase {
         return new TestSuite( SeqTest.class );
     }
 
-    public void testObjectivism() {
-        assertThat("A", equalTo("A"));
+    /** jumble arrays for mutations */
+    private static final int[] JUMBLE1 = new int[] { 67, 61, 96, 65, 56, 95, 2, 75, 17, 33, 87, 41, 16, 12, 15, 94, 6, 68, 0, 49, 38, 7, 34, 51, 92, 5, 24, 13, 45, 44, 93, 14, 11, 27, 74, 29, 88, 76, 19, 90, 8, 66, 80, 82, 50, 4, 25, 46, 70, 91};
+    private static final int[] JUMBLE2 = new int[] { 85, 60, 9, 58, 52, 72, 54, 39, 20, 81, 30, 86, 10, 69, 77, 26, 47, 35, 73, 89, 98, 53, 28, 36, 62, 84, 32, 42, 57, 97, 21, 63, 83, 59, 37, 79, 40, 48, 78, 31, 55, 3, 22, 43, 18, 71, 64, 1, 23, 99};
+    private static final int[] JUMBLE3 = new int[] { 3, 73, 47, 25, 57, 14, 31, 45, 26, 91, 13, 65, 7, 68, 61, 42, 98, 78, 37, 39, 79, 70, 54, 74, 40, 80, 6, 51, 52, 94, 86, 66, 33, 85, 81, 56, 0, 49, 44, 88, 12, 72, 16, 77, 35, 64, 59, 55, 9, 92};
+    private static final int[] JUMBLE4 = new int[] { 1, 67, 15, 58, 11, 97, 46, 43, 63, 20, 75, 89, 10, 96, 60, 41, 53, 87, 71, 90, 93, 48, 29, 22, 76, 82, 84, 32, 38, 4, 24, 19, 69, 50, 5, 62, 17, 18, 21, 8, 34, 27, 95, 83, 2, 28, 30, 23, 36, 99};
+    private static final int[] JUMBLE5 = new int[] { 63, 2, 36, 47, 52, 61, 37, 80, 5, 98, 46, 39, 49, 57, 88, 17, 86, 32, 74, 25, 87, 48, 34, 95, 97, 27, 77, 13, 78, 66, 28, 53, 9, 3, 70, 60, 21, 11, 7, 24, 59, 31, 69, 73, 94, 76, 91, 43, 20, 89};
+    private static final int[][] JUMBLES = new int[][] { JUMBLE1, JUMBLE2, JUMBLE3, JUMBLE4, JUMBLE5 };
+
+    public void testSeqHash() {
+        ProteinKmers.setKmerSize(8);
+        // We will test using a set of protein kmers.  There will be five very different starting proteins
+        // of length 100. For each, we will generate a set of mutations of 10, 20, 30, 40, and 50 amino acids.
+        // These are put into the hash, and then we ask for the closest N proteins of each starting protein.
+        // This is a crude test, but is a good place to start.
+        String p1 = "MDIQITHQVTEFDKEELLAGLRSYNAQFVDFSKNGQLGVYCRNESGEMVGGLIADRKGPWLCIDYLWVSESARNCGLGSKLMAMAEKEGLRKGCAHGLVD";
+        String p2 = "MSKDEISYQILYRYSLEKLYSTLTRRVDNVLSFALIFLGVGVTINVGSPFILGPGIVGIAILKRVLRFGTRSAQADRQSRAWLKLFNTQHRFPSDKTLFL";
+        String p3 = "MELQLMLNHFFERVRKDANFNAFLIDLEYNNIAYYIYFVATGNVKIITHAGHFISIKSNRKLIKVNSTPNTQLIKLTSDKHFSGEHSYEKYCTDLATAGV";
+        String p4 = "MTNITLSTQHYRIHRSDVEPVKEKTTEKDIFAKSITAVRNSFISLSTSLSDRFSLHQQTDIPTTHFHRGSASEGRAVLTSKTVKDFMLQKLNSLDIKGNA";
+        String p5 = "MSKDPAYARQTCEAILSAVYSNNKDHCCKLLISKGVSITPFLKEIGEAAQNAGLPGEIKNGVFTPGGAGANPFVVPLIASASIKYPHMFINHNQQVSFKA";
+        // Test the distances of the original 5.
+        ProteinKmers k1 = new ProteinKmers(p1);
+        ProteinKmers k2 = new ProteinKmers(p2);
+        ProteinKmers k3 = new ProteinKmers(p3);
+        ProteinKmers k4 = new ProteinKmers(p4);
+        ProteinKmers k5 = new ProteinKmers(p5);
+        ProteinKmers[] kArray = new ProteinKmers[] { k1, k2, k3, k4, k5 };
+        for (int i = 0; i < 4; i++)
+            for (int j = i + 1; j < 5; j++)
+                assertThat(kArray[i].distance(kArray[j]), greaterThan(0.99));
+        String[] prots = new String[] { p1, p2, p3, p4, p5 };
+        // Create the hash.
+        LSHSeqHash hash = new LSHSeqHash(200, 15, 20, 142857);
+        // Now create the mutations.
+        for (int i = 0; i < 5; i++) {
+            String p = prots[i];
+            int[] jumble = JUMBLES[i];
+            // This will be the starting point of the current mutation block.
+            int mutations = 0;
+            while (mutations <= 46) {
+                int limit = mutations + 4;
+                for (int k = mutations; k < limit; k++)
+                    p = mutate(p, jumble[k]);
+                mutations = limit;
+                String label = String.format("p%d.%04d", i, mutations);
+                ProteinKmers kmers = new ProteinKmers(p);
+                double dist = kmers.distance(kArray[i]);
+                double skDist = hash.testSketches(kmers, kArray[i]);
+                assertThat(label, Math.abs(dist - skDist), lessThan(0.1));
+                hash.add(kmers, label);
+            }
+        }
+        // Get the closest 5 to each original protein.
+        for (int i = 0; i < 5; i++) {
+            LSHSeqHash.Result[] r1 = new LSHSeqHash.Result[8];
+            String prefix = "p" + Integer.toString(i) + ".";
+            SortedSet<LSHSeqHash.Result> rSet = hash.getClosest(kArray[i], 8, 0.99);
+            assertThat(prefix, rSet.size(), lessThanOrEqualTo(8));
+            assertThat(prefix, rSet.size(), greaterThanOrEqualTo(3));
+            r1 = rSet.toArray(r1);
+            for (int k = 0; k < r1.length - 1; k++) {
+                LSHSeqHash.Result r = r1[k];
+                if (r != null) {
+                    LSHSeqHash.Result r0 = r1[k+1];
+                    if (r0 != null)
+                        assertThat(r.getDistance(), lessThanOrEqualTo(r0.getDistance()));
+                    assertThat(r.getDistance(), lessThanOrEqualTo(0.99));
+                    assertThat(r.getTarget().substring(0, 3), equalTo(prefix));
+                }
+            }
+        }
+    }
+
+
+    private static final String AA_LIST = "GPAVLIMCFYWHKRQNEDSTG";
+
+    /**
+     * Mutate a single amino acid in a protein string.
+     *
+     * @param prot	protein string to mutate
+     * @param pos	position of the amino acid to mutate
+     *
+     * @return the mutated protein
+     */
+    private String mutate(String prot, int pos) {
+        char[] acids = prot.toCharArray();
+        char aa = acids[pos];
+        acids[pos] = AA_LIST.charAt(AA_LIST.indexOf(aa) + 1);
+        String retVal = String.valueOf(acids);
+        return retVal;
     }
 
 }
