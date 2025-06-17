d97679d92bf75c0e12488d7fe26256d38ad0b967
Bruce Parrello <bparrello@figresearch.com>
2025-06-17 13:48:45 -0500

    Fixed warnings detected by VSCode

--- src/main/java/org/theseed/sequence/SequenceInputStream.java
+++ src/main/java/org/theseed/sequence/SequenceInputStream.java
@@ -3,7 +3,6 @@
  */
 package org.theseed.sequence;
 
-import java.io.Closeable;
 
 /**
  * This is a sequence stream attached to a file.  It includes closability.
@@ -11,7 +10,7 @@ import java.io.Closeable;
  * @author Bruce Parrello
  *
  */
-public interface SequenceInputStream extends SequenceStream, Closeable, AutoCloseable {
+public interface SequenceInputStream extends SequenceStream, AutoCloseable {
 
     public void close();
 
